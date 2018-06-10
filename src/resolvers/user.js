94d6115abc418017f24ebb852ce8c8fdc4cf1519
Elias Johansson <elias_johansson@hotmail.se>
2018-06-10 21:44:38 +0200

    Update and delete users

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -2,13 +2,30 @@ import { isAuthenticated } from '../utils/permissions';
 
 const authRequired = isAuthenticated.createResolver;
 
-// RESOLVERS
+// Queries
 const getUsers = authRequired((parent, args, { models }) => models.User.find());
 const getUserById = authRequired((parent, args, { models }) => models.User.findOne(args));
+
+// Mutations
+const updateUser = authRequired(async (parent, args, { models, user }) => {
+  const userToUpdate = await models.User.findOne({ _id: user });
+  userToUpdate.set({
+    username: args.username,
+  });
+  return userToUpdate.save();
+});
+const deleteUser = authRequired(async (parent, args, { models, user }) => {
+  const userToDelete = await models.User.findOne({ _id: user });
+  return userToDelete.remove();
+});
 
 export default {
   Query: {
     getUsers,
     getUserById,
+  },
+  Mutation: {
+    updateUser,
+    deleteUser,
   },
 };
