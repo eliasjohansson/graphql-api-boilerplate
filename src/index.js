8643d2cd0b7a985e1f1e93d1da56003446f32d38
Elias Johansson <elias_johansson@hotmail.se>
2018-10-01 09:26:31 +0200

    Migrated to Apollo Server v2 & added subscription support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import http from 'http';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { createServer } from 'http';
+import { ApolloServer } from 'apollo-server-express';
+import { PubSub } from 'graphql-subscriptions';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
@@ -10,28 +11,36 @@ import { PORT, MONGO_URI } from './utils/dotenv';
 import { addUserToReq } from './utils/auth';
 import models from './models';
 
+export const pubsub = new PubSub();
+
 const app = express();
-const server = http.Server(app);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(addUserToReq);
 
-app.use(
-  '/graphql',
-  graphqlExpress(req => ({
-    formatError,
-    schema,
-    context: {
+const apollo = new ApolloServer({
+  formatError,
+  schema,
+  context: async ({ req, connection }) => {
+    if (connection) {
+      return {};
+    }
+    return {
       user: req.user,
       models,
-    },
-  })),
-);
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+    };
+  },
+});
 
+apollo.applyMiddleware({ app, path: '/graphql' });
+
+// Create HTTP server with Subscriptions
+const server = createServer(app);
+apollo.installSubscriptionHandlers(server);
 
 server.listen(PORT, () => {
   mongoose.connect(MONGO_URI);
-  console.log(`Server running on localhost:${PORT}...`);
+  console.log(`🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}`);
+  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${apollo.subscriptionsPath}`);
 });
